e28d297f913bf3847b98552f0b6a754f335e4ba5
juanf <yioyiomain@gmail.com>
2024-09-27 12:07:08 -0500

    Actualizacion de  las cards

--- components/organisms/Card.tsx
+++ components/organisms/Card.tsx
@@ -8,17 +8,19 @@ type CardProps = {
 
 export function Card({ character }: CardProps) {
   return (
-    <article className="flex flex-col h-max rounded-sm border-slate-400 p-4 w-[350px]">
-      <Link href={`/${character.id}`}>
+    <article className="relative flex flex-col h-max rounded-sm border-slate-400 p-4 w-[350px] overflow-hidden">
+      <Link href={`/${character.id}`} className="relative block">
         <Image
-          className="rounded-md"
+          className="rounded-md transition-transform duration-300 transform hover:scale-105"
           src={`http://localhost:300/imagenes/${character.imagen}`}
           alt={character.nombre}
           width={250}
           height={200}
         />
+        <div className="absolute inset-0 bg-black bg-opacity-70 flex items-center justify-center opacity-0 transition-opacity duration-300 hover:opacity-100">
+          <p className="text-white font-semibold text-xl">{character.nombre}</p>
+        </div>
       </Link>
-      <p className="font-semibold text-xl text-gray-950">{character.nombre}</p>
     </article>
   );
 }
