d05eeeed7543f9195300a56232a65578a3441772
juanf <yioyiomain@gmail.com>
2024-09-30 12:34:43 -0500

    Actualizacion

--- app/zenless-zone-zero/characters/[slug]/page.tsx
+++ app/zenless-zone-zero/characters/[slug]/page.tsx
@@ -1,6 +1,8 @@
 import type { Metadata, ResolvingMetadata } from 'next'
 import Image from 'next/image'
-import ben from "@/assets/images/Ben.png"
+import { getCharacterById } from '@/libs/api_general'
+import { bg_blue_30 } from '@/components/tokens'
+
 
 type Props = {
   params: { slug: string }
@@ -11,24 +13,32 @@ export async function generateMetadata(
   { params, searchParams }: Props,
   parent: ResolvingMetadata
 ): Promise<Metadata> {
- 
+
+  const character = await getCharacterById(parseInt(params.slug))  
+
   return {
-    title: `characters list ${params.slug}`,
-    description: "this is a tier list",
+    title: `character ${character[0].nombre}`,
+    description: `Zenless Zone Zero ${character[0].nombre}`,
   }
 }
  
 
-export default function Page({ params }: { params: { slug: string } }) {
-    return (
+export default async function Page({ params }: { params: { slug: string } }) {
+  const character = await getCharacterById(parseInt(params.slug))  
+  return (
         <>
-            <Image
-                alt='asdf'
-                src={ben}
+          <main className={`flex flex-col max-w-5xl mx-auto h-screen p-4 mt-4 ${bg_blue_30}`}>
+            <div className="flex items-center">
+              <Image
+                alt={character[0].nombre}
+                src={`http://localhost:300/imagenes/${character[0].imagen}`}
+                width={250}
+                height={250}
                 className='rounded-full'
-            />
-              
-            <p>{params.slug}</p>
+              />
+              <p className="text-xl font-semibold text-white ml-4">{character[0].nombre}</p>
+            </div>
+          </main>
         </>
 
     )
--- libs/api_general.ts
+++ libs/api_general.ts
@@ -6,3 +6,10 @@ export const getAllCharacters = async () => {
     const { data } = rawData
     return data
 }
+
+export const getCharacterById = async (id:number) => {
+    const info = await fetch(`${API}/zenless-zone-zero/characters/${id}`)
+    const rawData = await info.json()
+    const { data } = rawData
+    return data
+}
