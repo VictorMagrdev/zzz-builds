8b171016dd044986f8ea14ee271179c7e39a4ee2
juanf <yioyiomain@gmail.com>
2024-10-24 14:55:29 -0500

    Reparando el login con autenticacion

--- components/screens/ProfileView.tsx
+++ components/screens/ProfileView.tsx
@@ -1,21 +1,35 @@
 'use client'
 import { bg_blue_60,bg_blue_30 } from '@/components/tokens'
 import { ProxyInfo } from "@/components";
-import Ben from "@/assets/images/Ben.webp"
-import { useEffect } from "react";
-import { useState } from "react";
 import { getUserProfile } from "@/libs/api_general";
 import { Profile } from '@/types/api-general';
+import useStore from '@/store/useStore';
+import { useEffect, useState } from 'react';
 
+export default function ProfileView() {
+    const token = useStore((store) => store.token);
+    const [profile, setProfile]:any = useState(null);
 
-export default function ProfileView({profile}:any) {
-  
+    useEffect(() => {
+        const fetchProfile = async () => {
+            try {
+                let data = await getUserProfile(token);
+                setProfile(data);
+            } catch (error) {
+                console.error("Failed to fetch profile:", error);
+            }
+        };
+
+        fetchProfile();
+    }, [token]);
+
+  if (!profile) return <div>Loading...</div>
     return (
         <div className={`flex flex-col ${bg_blue_60} space-y-4 min-h-screen`} >
             <div className="m-8 grid grid-rows-2 grid-flow-col gap-x-0 gap-y-4">
                 <ProxyInfo
                     imagen={profile.img_profile}
-                    username="Usuario123"
+                    username={profile.user}
                     nivel={5}
                     uid="12345"
                 />
