93c43760fb21aebd39b127385971e4e50d6a97e1
JuanFosoA <yioyiomain@gmail.com>
2024-12-06 10:30:50 -0500

    ignore eslint

--- next.config.mjs
+++ next.config.mjs
@@ -4,17 +4,20 @@ const withNextIntl = createNextIntlPlugin('./i18n/request.ts');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-
   images: {
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'zzzbuilds-server.lat',
         port: '', 
-        pathname: '/imagenes/**',
+        pathname: '/imagenes/**', 
       },
     ],
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
   },
 };
 
 export default withNextIntl(nextConfig);
+
