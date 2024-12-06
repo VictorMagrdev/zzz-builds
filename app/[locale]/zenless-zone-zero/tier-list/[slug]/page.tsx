9eb1cab1ee0a2f8ab93584a76e55d2440f35dd40
JuanFosoA <yioyiomain@gmail.com>
2024-12-06 11:18:09 -0500

    Arreglando metadata slug guides

--- app/[locale]/zenless-zone-zero/tier-list/[slug]/page.tsx
+++ app/[locale]/zenless-zone-zero/tier-list/[slug]/page.tsx
@@ -1,25 +1,27 @@
 import TierListSlug from '@/components/screens/TierListSlug'
 import type { Metadata, ResolvingMetadata } from 'next'
- 
-type Props = {
-  params: { slug: string }
-  searchParams: { [key: string]: string | string[] | undefined }
+
+type tParams = Promise<{ slug: string }>;
+
+type PageProps = {
+  params: tParams;
+  searchParams: { [key: string]: string | string[] | undefined };
 }
- 
+
+
 export async function generateMetadata(
-  { params, searchParams }: Props,
+  props: PageProps,
   parent: ResolvingMetadata
 ): Promise<Metadata> {
-  const { slug } =  await params;
+  const { slug } = await props.params;  
   return {
     title: `Tier list ${slug}`,
     description: "this is a tier list",
-  }
+  };
 }
- 
 
-export default async function Page({ params }: { params: { slug: string  } }) {
-  const { slug } = await params;
+export default async function Page(props: PageProps) {
+  const { slug } = await props.params; 
   if (!slug) {
     return (
       <main className="flex items-center justify-center h-screen">
@@ -28,5 +30,5 @@ export default async function Page({ params }: { params: { slug: string  } }) {
     );
   }
   const tierlistId = parseInt(slug);
-    return <TierListSlug tierlistId={tierlistId} />
-  }
+  return <TierListSlug tierlistId={tierlistId} />;
+}
