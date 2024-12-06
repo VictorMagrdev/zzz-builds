0a340541364093032b37b6c11d02bcddffb69057
JuanFosoA <yioyiomain@gmail.com>
2024-12-06 10:38:16 -0500

    Correccion characters metadata

--- app/[locale]/zenless-zone-zero/characters/[slug]/page.tsx
+++ app/[locale]/zenless-zone-zero/characters/[slug]/page.tsx
@@ -2,7 +2,6 @@ import type { Metadata, ResolvingMetadata } from 'next'
 import { getCharacterById } from '@/libs/api_general'
 import CharacterView from '@/components/molecules/CharacterView'
 
-
 type Props = {
   params: { slug: string }
   searchParams: { [key: string]: string | string[] | undefined }
@@ -12,7 +11,7 @@ export async function generateMetadata(
   { params, searchParams }: Props,
   parent: ResolvingMetadata
 ): Promise<Metadata> {
-  const { slug } = await params;
+  const { slug } = params; 
   const character = await getCharacterById(parseInt(slug))
   return {
     title: `Character ${character[0].nombre}`,
@@ -21,7 +20,7 @@ export async function generateMetadata(
 }
 
 export default async function Page({ params }: { params: { slug: string } }) {
-  const { slug } = await params;
+  const { slug } = params; 
   const character = await getCharacterById(parseInt(slug))
   return (
     <CharacterView character={character} />
